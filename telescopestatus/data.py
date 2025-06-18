3148662eb22280c03c1f3e26c7055be2a038e8bc
Britney Whittington <bwhitt7@students.towson.edu>
2025-06-18 00:49:16 -0400

    More comments & documentation

--- telescopestatus/data.py
+++ telescopestatus/data.py
@@ -12,7 +12,7 @@ class TelescopeData:
     Stores, generates, and visualizes MAST data for a given telescope (JWST, HST, or TESS).
     """
     
-    def __init__(self, telescope: str, start_time: Time = default_min_year, end_time: Time = "now", max_data: int = None, token: str = None, data_file_path: str = None, data_file_type: str = None):
+    def __init__(self, telescope: str, start_time: Time = None, end_time: Time = None, max_data: int = None, token: str = None, data_file_path: str = None, data_file_type: str = None):
         """
         If no TelescopeData data file is given, it will automatically call fetch_telescope_data() with given parameters.
         telescope, max_data, start_time, and end_time become useless when data is given manually, but telescope, start_time, and end_time should be filled out for usage in the graphs.
@@ -27,7 +27,7 @@ class TelescopeData:
             data_file_type (str, optional): The file type of the TelescopeData data file. Should be a file type parsable by pandas (csv, json, pickles, etc)
         """
         self.telescope = None
-        self.start_time = default_min_year
+        self.start_time = default_min_year # start and end time should *always* be set during initialization, and never set to none.
         self.end_time = Time.now()
         self.data = None
         self.set_data(telescope, start_time, end_time, max_data, token, data_file_path=data_file_path, data_file_type=data_file_type)
@@ -55,6 +55,7 @@ class TelescopeData:
             if not self.telescope in [t.upper() for t in Observations.list_missions()]:
                 raise ValueError(f"Invalid telescope/mission name given. Please use {", ".join(Observations.list_missions())}.")
         
+        # validate and convert the time values if given. otherwise, just continue using the current start and end times.
         start_time = self.validate_and_convert_time_param(start_time) if start_time else self.start_time
         end_time = self.validate_and_convert_time_param(end_time) if end_time else self.end_time
         
@@ -63,6 +64,7 @@ class TelescopeData:
             if start_time > end_time:
                 raise ValueError(f"start_time should be less than end_time.")
         
+        # set start and end if successful
         self.start_time = start_time
         self.end_time = end_time
         
@@ -70,7 +72,7 @@ class TelescopeData:
         
         # TelescopeData file given
         if data_file_path:
-            if not data_file_type:
+            if not data_file_type: # data file type and path should always be given together for the read_data method
                 raise ValueError("data_file_type should be given in addition to data_file_path.")
             try:
                 self.read_data(file_path=data_file_path, file_type=data_file_type)
@@ -85,9 +87,20 @@ class TelescopeData:
 
 
     def validate_and_convert_time_param(self, time) -> Time:
+        """Make sure the given time value is convertible to astro.time.Time, and do so if possible.
+
+        Args:
+            time (any): Value representing time, which will be converted to astro.time.Time if possible. Should be a string in a proper Time format (such as iso), datetime, or Time.
+
+        Raises:
+            ValueError: Improper format for time.
+
+        Returns:
+            Time: The given value converted to astro.time.Time for calculations.
+        """
         if isinstance(time,str) or isinstance(time,datetime):
-            if time == "now":
-                return Time.now()
+            if isinstance(time,str) and time == "now":
+                return Time.now() # quickly return the current time without needing to use Time.now()
             try:
                 return Time(time)
             except Exception:
@@ -95,7 +108,7 @@ class TelescopeData:
         elif isinstance(time,Time):
             return time
         else:
-            raise ValueError(f"{time} -> Improper format for time.")
+            raise ValueError(f"{time} -> Improper format for time (not str, datetime, or Time).")
 
 
     def fetch_telescope_data(self, max_data: int = None, token: str = None) -> pd.DataFrame:
@@ -123,9 +136,9 @@ class TelescopeData:
         print(f"[ Loading {self.telescope} data from MAST... ]")
         try:
             obs = Observations.query_criteria(
-                obs_collection=[self.telescope],
-                t_min=[self.start_time.mjd, self.end_time.mjd],
-                page = 1 if max_data else None,
+                obs_collection=[self.telescope], # search collection for telescope
+                t_min=[self.start_time.mjd, self.end_time.mjd], # converts start and end times to mjd for searching
+                page = 1 if max_data else None, #set page and page size if we have a max data amount we want to search for
                 pagesize = max_data if max_data else None
             )
             
@@ -135,18 +148,30 @@ class TelescopeData:
             return self.data
         
         except Exception as e:
-            # May get memory errors with low memory
+            # May get memory errors with low memory! be careful, and maybe run some garbage collection?
             print("[ Error getting data ]")
             raise e
     
     
-    def export_data(self, file_name: str = "telescope_data.csv", file_type: str = "csv"):
+    def export_data(self, file_name: str, file_type):
+        """Export currently loaded MAST data into csv or pickle.
+
+        Args:
+            file_name (str): The file name.
+            file_type (str): The file type. Should be csv or pickle.
+        """
         if file_type == "csv":
             self.data.to_csv(file_name)
         elif file_type == "pickle":
             self.data.to_pickle(file_name)
     
-    def read_data(self, file_path, file_type):
+    def read_data(self, file_path: str, file_type: str):
+        """Reads exported data, either csv or pickle generated by export_data.
+
+        Args:
+            file_path (str): The path and name of the file.
+            file_type (str): The file type. Should be csv or pickle.
+        """
         if file_type == "csv":
             data = pd.read_csv(file_path)
         elif file_type == "pickle":
@@ -154,18 +179,47 @@ class TelescopeData:
         self.data = data
         
         
-    def chart(self, type, data_type, options={}):
-        if type=="pie":
+    def chart(self, chart_type: str, data_type: str, options={}):
+        """Super function that is called for generating charts/graphs.
+        Available charts:
+        - pie -> instruments, dataproducts
+        - hist -> exptime
+        
+        Available options:
+        - hist:
+            - log_scale: Whether or not the chart has log scale in the y_axis
+        
+        Args:
+            chart_type (str): What type of chart you want to generate.
+            data_type (_type_): The type of data you want to load for the charts.
+            options (dict, optional): Extra options that get fed into the charts. Defaults to {}.
+
+        Returns:
+            Figure: A plotly Figure
+        """
+        if chart_type=="pie":
             if data_type=="instruments":
                 return self._pie_chart("instrument_name", "Instrument", f"Instrument Usage in {self.telescope.upper()} Observations", options)
             elif data_type=="dataproducts":
                 return self._pie_chart("dataproduct_type", "Data Product Type", f"Data Product Type of {self.telescope.upper()} Observations", options)
-        elif type=="hist":
+        elif chart_type=="hist":
             if data_type=="exptime":
                 return self._hist("t_exptime", "Exposure Length", f"Exposure Length of {self.telescope.upper()} Observations", options)
     
     
+    
     def _pie_chart(self, column:str, column_name:str, title:str, options={}):
+        """Returns a plotly pie chart
+
+        Args:
+            column (str): The column of the MAST data table to query.
+            column_name (str): Label for the data.
+            title (str): Title used for the graph.
+            options (dict, optional): Extra options to use. Currently unused for piecharts. Defaults to {}.
+
+        Returns:
+            Figure: A plotly Figure
+        """
         counts = self.data[column].value_counts()
         df_result = pd.DataFrame(counts).reset_index() 
         start_time = self.start_time.to_value("iso", subfmt="date")
@@ -178,19 +232,24 @@ class TelescopeData:
             labels={column: column_name, "count": "# of Observations"},
             title=f"{title} (between {start_time} and {end_time})"
         )
-        fig.update_traces(textposition='inside')
-        fig.update_layout(uniformtext_minsize=12, uniformtext_mode='hide')
-        fig.update_layout(margin=dict(t=50, b=20, l=0, r=0))
+        fig.update_traces(textposition='inside') # put all labels in pie slices
+        fig.update_layout(uniformtext_minsize=12, uniformtext_mode='hide') # hide labels if text is too small
+        fig.update_layout(margin=dict(t=50, b=20, l=0, r=0)) # modify margins for better look
         return fig
     
-    def _hist(self,column: str, column_name: str, title:str, options={}):
-        """Prints a Plotly histogram.
+    
+    
+    def _hist(self, column: str, column_name: str, title:str, options={}):
+        """Returns a plotly histogram.
 
         Args:
-            log_scale (bool, optional): Display graph in log scale. Defaults to False.
+            column (str): The column of the MAST data table to query.
+            column_name (str): Label for the data.
+            title (str): Title used for the graph.
+            options (dict, optional): Extra options to use (log_scale). Defaults to {}.
 
         Returns:
-            Figure: Plotly figure.
+            Figure: A plotly Figure
         """
         start_time = self.start_time.to_value("iso", subfmt="date")
         end_time = self.end_time.to_value("iso", subfmt="date")
